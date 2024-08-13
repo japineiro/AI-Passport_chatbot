859233484eebf344c315c063d5f9f290497fda96
amreenapathan <pathan.amreena@ufl.edu>
2024-08-13 11:21:24 -0400

    Update App-v2.py

--- App-v2.py
+++ App-v2.py
@@ -13,10 +13,8 @@ st.set_page_config(
     page_title="AI Passport Chatbot",
     page_icon="assistant"
 )
-
-os.environ['OPENAI_API_KEY'] = 'sk-proj-UVeQodPPxVwv0tkDy3BbT3BlbkFJJazWhRRP3uyLFIdfcXmg'
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
-
 # Function to encode the image
 def encode_image(image_file):
     return base64.b64encode(image_file.read()).decode('utf-8')
@@ -24,7 +22,7 @@ def encode_image(image_file):
 # Define LLM model
 def generate_response(prompt, image=None, pdf=None):
     if image:
-        base64_image = encode_image(uploaded_image)
+        base64_image = encode_image(uploaded_file)
         response = client.chat.completions.create(
             model="gpt-4o",
             messages=[
@@ -44,13 +42,14 @@ def generate_response(prompt, image=None, pdf=None):
         )
         return response.choices[0].message.content.strip()
     elif pdf:
-        reader = PdfReader(uploaded_text_file)
+        reader = PdfReader(uploaded_file)
         page = reader.pages[0]
         text = page.extract_text()
 
         response = client.chat.completions.create(
             model="gpt-4o",
             messages=[
+                {"role": "system", "content": "You are a chatbot with professional knowledge in medicine."},
                 {
                 "role": "user",
                 "content": [
@@ -68,7 +67,10 @@ def generate_response(prompt, image=None, pdf=None):
     else:    
         response = client.chat.completions.create(
             model="gpt-4o",
-            messages=[{"role": "user", "content": prompt}],
+            messages=[
+                {"role": "system", "content": "You are a chatbot with professional knowledge in medicine."},
+                {"role": "user", "content": prompt}
+            ],
             temperature=0.3,
             max_tokens=4096
         )
@@ -85,14 +87,14 @@ st.markdown("***Chat History***")
 for i, message in enumerate(reversed(st.session_state.messages)):
     if message["role"] == "user":
         st.markdown(f"<span style='color: blue;'>**You:** {message['content']}</span>", unsafe_allow_html=True)
-        if st.button("📋", key=f"copy_user_{i}"):
-            pyperclip.copy(message["content"])
-            st.success("Copied to clipboard!")
+        # if st.button("📋", key=f"copy_user_{i}"):
+        #     pyperclip.copy(message["content"])
+        #     st.success("Copied to clipboard!")
     elif message["role"] == "assistant":
         st.markdown(f"<span style='color: green;'>**Assistant:** {message['content']}</span>", unsafe_allow_html=True)
-        if st.button("📋", key=f"copy_assistant_{i}"):
-            pyperclip.copy(message["content"])
-            st.success("Copied to clipboard!")
+        # if st.button("📋", key=f"copy_assistant_{i}"):
+        #     pyperclip.copy(message["content"])
+        #     st.success("Copied to clipboard!")
 # Export conversation button
 if st.session_state.messages:
     chat_history = "\n".join([f"{msg['role'].capitalize()}: {msg['content']}" for msg in st.session_state.messages])
@@ -103,21 +105,29 @@ if st.session_state.messages:
         mime='text/plain'
     )
 
+with st.sidebar:
+    st.subheader("Attach your files.")
+    # Streamlit file uploader to upload an image
+    uploaded_file = st.file_uploader("Upload an Image", type=["pdf","jpg", "jpeg", "png"])
+
+
 # User input and response form
 with st.form(key='response_form'):
     user_input = st.text_area('Enter your question here:', 'How can I help you today?', label_visibility='collapsed')
     submit_button = st.form_submit_button("Respond")
-    # Streamlit file uploader to upload an image
-    uploaded_image = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
-    uploaded_text_file = st.file_uploader("Upload a PDF", type=["pdf"])
 
     if submit_button and user_input:
-        if uploaded_image:
-            response = generate_response(user_input, image=uploaded_image)
-        elif uploaded_text_file:
-            response = generate_response(user_input, pdf=uploaded_text_file)
+        if uploaded_file is not None:
+            file_type = uploaded_file.type
+            if (file_type == "application/pdf"):
+                response = generate_response(user_input, pdf=uploaded_file)
+            if (file_type == "image/jpeg" or file_type == "image/png"):
+                response = generate_response(user_input, image=uploaded_file)
+            st.session_state.messages.append({"role": "user", "content": user_input})
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.rerun()
         else:
             response = generate_response(user_input)
-        st.session_state.messages.append({"role": "user", "content": user_input})
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        st.rerun()
+            st.session_state.messages.append({"role": "user", "content": user_input})
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.rerun()
